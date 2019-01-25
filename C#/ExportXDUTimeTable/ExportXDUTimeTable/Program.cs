c9edcf22a9fd13a1fdd34ef67a034cc41d0f20ac
Robotxm <wanglinpei65@gmail.com>
2019-01-26 01:41:52 +0800

    Rename Form1 to frmMain

--- C#/ExportXDUTimeTable/ExportXDUTimeTable/Program.cs
+++ C#/ExportXDUTimeTable/ExportXDUTimeTable/Program.cs
@@ -16,7 +16,7 @@ namespace ExportXDUTimeTable
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
