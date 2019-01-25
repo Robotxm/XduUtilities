b1638310a77b8426f3528c8b171ed519500a0334
Robotxm <wanglinpei65@gmail.com>
2019-01-25 23:15:10 +0800

    Initialize repo

---
+++ C#/ExportXDUTimeTable/ExportXDUTimeTable/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace ExportXDUTimeTable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblStuID = new System.Windows.Forms.Label();
+            this.lblPwd = new System.Windows.Forms.Label();
+            this.lblVerify = new System.Windows.Forms.Label();
+            this.txtStuID = new System.Windows.Forms.TextBox();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.txtVerify = new System.Windows.Forms.TextBox();
+            this.picVerify = new System.Windows.Forms.PictureBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblNotice = new System.Windows.Forms.Label();
+            this.chkChangeTime = new System.Windows.Forms.CheckBox();
+            this.lblTimeA = new System.Windows.Forms.Label();
+            this.dtpTimeA = new System.Windows.Forms.DateTimePicker();
+            this.lblTimeB = new System.Windows.Forms.Label();
+            this.dtpTimeB = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.picVerify)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblStuID
+            // 
+            this.lblStuID.AutoSize = true;
+            this.lblStuID.Location = new System.Drawing.Point(12, 24);
+            this.lblStuID.Name = "lblStuID";
+            this.lblStuID.Size = new System.Drawing.Size(88, 30);
+            this.lblStuID.TabIndex = 0;
+            this.lblStuID.Text = "学号:";
+            // 
+            // lblPwd
+            // 
+            this.lblPwd.AutoSize = true;
+            this.lblPwd.Location = new System.Drawing.Point(12, 72);
+            this.lblPwd.Name = "lblPwd";
+            this.lblPwd.Size = new System.Drawing.Size(88, 30);
+            this.lblPwd.TabIndex = 1;
+            this.lblPwd.Text = "密码:";
+            // 
+            // lblVerify
+            // 
+            this.lblVerify.AutoSize = true;
+            this.lblVerify.Location = new System.Drawing.Point(12, 120);
+            this.lblVerify.Name = "lblVerify";
+            this.lblVerify.Size = new System.Drawing.Size(118, 30);
+            this.lblVerify.TabIndex = 2;
+            this.lblVerify.Text = "验证码:";
+            this.lblVerify.Visible = false;
+            // 
+            // txtStuID
+            // 
+            this.txtStuID.Location = new System.Drawing.Point(136, 12);
+            this.txtStuID.Name = "txtStuID";
+            this.txtStuID.Size = new System.Drawing.Size(269, 42);
+            this.txtStuID.TabIndex = 3;
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(136, 60);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(269, 42);
+            this.txtPwd.TabIndex = 4;
+            this.txtPwd.UseSystemPasswordChar = true;
+            // 
+            // txtVerify
+            // 
+            this.txtVerify.Location = new System.Drawing.Point(136, 108);
+            this.txtVerify.Name = "txtVerify";
+            this.txtVerify.Size = new System.Drawing.Size(138, 42);
+            this.txtVerify.TabIndex = 5;
+            this.txtVerify.Visible = false;
+            // 
+            // picVerify
+            // 
+            this.picVerify.Location = new System.Drawing.Point(280, 108);
+            this.picVerify.Name = "picVerify";
+            this.picVerify.Size = new System.Drawing.Size(125, 57);
+            this.picVerify.TabIndex = 6;
+            this.picVerify.TabStop = false;
+            this.picVerify.Visible = false;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(278, 171);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(127, 50);
+            this.btnLogin.TabIndex = 7;
+            this.btnLogin.Text = "获取";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lblNotice
+            // 
+            this.lblNotice.AutoSize = true;
+            this.lblNotice.Location = new System.Drawing.Point(12, 224);
+            this.lblNotice.Name = "lblNotice";
+            this.lblNotice.Size = new System.Drawing.Size(1033, 240);
+            this.lblNotice.TabIndex = 8;
+            this.lblNotice.Text = resources.GetString("lblNotice.Text");
+            // 
+            // chkChangeTime
+            // 
+            this.chkChangeTime.AutoSize = true;
+            this.chkChangeTime.Checked = true;
+            this.chkChangeTime.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkChangeTime.Location = new System.Drawing.Point(411, 12);
+            this.chkChangeTime.Name = "chkChangeTime";
+            this.chkChangeTime.Size = new System.Drawing.Size(231, 34);
+            this.chkChangeTime.TabIndex = 9;
+            this.chkChangeTime.Text = "区分作息时间";
+            this.chkChangeTime.UseVisualStyleBackColor = true;
+            // 
+            // lblTimeA
+            // 
+            this.lblTimeA.AutoSize = true;
+            this.lblTimeA.Location = new System.Drawing.Point(412, 72);
+            this.lblTimeA.Name = "lblTimeA";
+            this.lblTimeA.Size = new System.Drawing.Size(358, 30);
+            this.lblTimeA.TabIndex = 10;
+            this.lblTimeA.Text = "春夏秋作息时间开始日期:";
+            // 
+            // dtpTimeA
+            // 
+            this.dtpTimeA.Location = new System.Drawing.Point(776, 60);
+            this.dtpTimeA.Name = "dtpTimeA";
+            this.dtpTimeA.Size = new System.Drawing.Size(267, 42);
+            this.dtpTimeA.TabIndex = 11;
+            this.dtpTimeA.Value = new System.DateTime(2019, 5, 2, 0, 0, 0, 0);
+            // 
+            // lblTimeB
+            // 
+            this.lblTimeB.AutoSize = true;
+            this.lblTimeB.Location = new System.Drawing.Point(412, 120);
+            this.lblTimeB.Name = "lblTimeB";
+            this.lblTimeB.Size = new System.Drawing.Size(328, 30);
+            this.lblTimeB.TabIndex = 12;
+            this.lblTimeB.Text = "冬季作息时间开始日期:";
+            // 
+            // dtpTimeB
+            // 
+            this.dtpTimeB.Location = new System.Drawing.Point(776, 108);
+            this.dtpTimeB.Name = "dtpTimeB";
+            this.dtpTimeB.Size = new System.Drawing.Size(267, 42);
+            this.dtpTimeB.TabIndex = 13;
+            this.dtpTimeB.Value = new System.DateTime(2018, 10, 8, 0, 0, 0, 0);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1085, 532);
+            this.Controls.Add(this.dtpTimeB);
+            this.Controls.Add(this.lblTimeB);
+            this.Controls.Add(this.dtpTimeA);
+            this.Controls.Add(this.lblTimeA);
+            this.Controls.Add(this.chkChangeTime);
+            this.Controls.Add(this.lblNotice);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.picVerify);
+            this.Controls.Add(this.txtVerify);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.txtStuID);
+            this.Controls.Add(this.lblVerify);
+            this.Controls.Add(this.lblPwd);
+            this.Controls.Add(this.lblStuID);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "导出课程表 - 西安电子科技大学";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picVerify)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStuID;
+        private System.Windows.Forms.Label lblPwd;
+        private System.Windows.Forms.Label lblVerify;
+        private System.Windows.Forms.TextBox txtStuID;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.TextBox txtVerify;
+        private System.Windows.Forms.PictureBox picVerify;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblNotice;
+        private System.Windows.Forms.CheckBox chkChangeTime;
+        private System.Windows.Forms.Label lblTimeA;
+        private System.Windows.Forms.DateTimePicker dtpTimeA;
+        private System.Windows.Forms.Label lblTimeB;
+        private System.Windows.Forms.DateTimePicker dtpTimeB;
+    }
+}
+
---
+++ C#/ExportXDUTimeTable/ExportXDUTimeTable/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+using Ical.Net;
+using Ical.Net.CalendarComponents;
+using Ical.Net.DataTypes;
+using Ical.Net.Serialization;
+using Newtonsoft.Json;
+
+namespace ExportXDUTimeTable
+{
+    public partial class Form1 : Form
+    {
+        HttpClient hc = new HttpClient();
+        string strLt, strExecution, strEventId, strRmShown;
+        Calendar timeTable = new Calendar();
+
+        // Time A (summer)
+        string[] sectionStartTime_A = {"08:00", "08:30", "09:20", "10:25", "11:15", // morning
+            "14:30", "15:20", "16:25", "17:15", // afternoon
+            "19:30", "20:20"}; // evening
+        string[] sectionEndTime_A   = {"08:30", "09:15", "10:05", "11:10", "12:00",
+            "15:15", "16:05", "17:10", "18:00",
+            "20:15", "21:05"};
+
+        // Time B (spring, autumn and winter)
+        string[] sectionStartTime_B = {"08:00", "08:30", "09:20", "10:25", "11:15",
+            "14:00", "14:50", "15:55", "16:45",
+            "19:00", "19:50"};
+        string[] sectionEndTime_B   = {"08:30", "09:15", "10:05", "11:10", "12:00",
+            "14:45", "15:35", "16:40", "17:30",
+            "19:45", "20:35"};
+
+        int allTeachWeeks;
+
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (!hc.DefaultRequestHeaders.Contains("Referer"))
+                hc.DefaultRequestHeaders.Add("Referer", "http://ids.xidian.edu.cn/authserver/login?service=https%3A%2F%2Fxidian.cpdaily.com%2Fcomapp-timetable%2Fsys%2FschoolTimetable%2Fv2%2Fapi%2FweekTimetable");
+            List<KeyValuePair<String, String>> paramList = new List<KeyValuePair<String, String>>
+            {
+                new KeyValuePair<string, string>("username", txtStuID.Text),
+                new KeyValuePair<string, string>("password", txtPwd.Text),
+                new KeyValuePair<string, string>("submit", ""),
+                new KeyValuePair<string, string>("lt", strLt),
+                new KeyValuePair<string, string>("execution", strExecution),
+                new KeyValuePair<string, string>("_eventId", strEventId),
+                new KeyValuePair<string, string>("rmShown", strRmShown)
+            };
+            // Get json which contains time table
+            string strJson = hc.PostAsync("http://ids.xidian.edu.cn/authserver/login?service=https%3A%2F%2Fxidian.cpdaily.com%2Fcomapp-timetable%2Fsys%2FschoolTimetable%2Fv2%2Fapi%2FweekTimetable", new FormUrlEncodedContent(paramList)).Result.Content.ReadAsStringAsync().Result;
+            if (!strJson.Contains("有误") || !strJson.Contains("放假中"))
+            {
+                JObject jCourse = (JObject)JsonConvert.DeserializeObject(strJson);
+
+                allTeachWeeks = int.Parse(jCourse["allTeachWeeks"].ToString());
+
+                // Start conversion
+                // Traverse all weeks in term
+                for (int curWeek = 0; curWeek < allTeachWeeks; curWeek++)
+                {
+                    // Traverse all days in a week
+                    for (int curDay = 0; curDay < 7; curDay++)
+                    {
+                        // Check if there's any course in the day
+                        if (jCourse["termWeeksCourse"][curWeek]["courses"][curDay]["sectionCourses"].Count() != 0)
+                        {
+                            // Traverse all courses in the day
+                            for (int curCourse = 0; curCourse < jCourse["termWeeksCourse"][curWeek]["courses"][curDay]["sectionCourses"].Count(); curCourse++)
+                            {
+                                string courseDay = jCourse["termWeeksCourse"][curWeek]["courses"][curDay]["date"].ToString();
+                                string courseName = jCourse["termWeeksCourse"][curWeek]["courses"][curDay]["sectionCourses"][curCourse]["courseName"].ToString();
+                                string courseClassroom = jCourse["termWeeksCourse"][curWeek]["courses"][curDay]["sectionCourses"][curCourse]["classroom"].ToString();
+                                int courseStart, courseEnd;
+                                courseStart = int.Parse(jCourse["termWeeksCourse"][curWeek]["courses"][curDay]["sectionCourses"][curCourse]["sectionStart"].ToString());
+                                courseEnd = int.Parse(jCourse["termWeeksCourse"][curWeek]["courses"][curDay]["sectionCourses"][curCourse]["sectionEnd"].ToString());
+                                if (chkChangeTime.Checked)
+                                {
+                                    if (DateTime.Compare(DateTime.Parse(courseDay), dtpTimeB.Value) > 0 && DateTime.Compare(DateTime.Parse(courseDay), dtpTimeA.Value) < 0) // Time B
+                                    {
+                                        AddToCalendar(courseDay, courseName, courseClassroom, courseStart, courseEnd, true);
+                                    }
+                                    else
+                                    {
+                                        AddToCalendar(courseDay, courseName, courseClassroom, courseStart, courseEnd, false);
+                                    }
+                                }
+                                else
+                                {
+                                    AddToCalendar(courseDay, courseName, courseClassroom, courseStart, courseEnd, false);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                CalendarSerializer serializer = new CalendarSerializer();
+                string serializedCalendar = serializer.SerializeToString(timeTable);
+                // Write to iCalendar file
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop).EndsWith("\\") ? Environment.GetFolderPath(Environment.SpecialFolder.Desktop) : Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\";
+                System.IO.File.WriteAllText(desktopPath + "xdu_timetable_" + txtStuID.Text + "_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".ics", serializedCalendar);
+                MessageBox.Show(this, "课表保存到: " + desktopPath + "xdu_timetable_" + txtStuID.Text + "_" + DateTime.Now.ToString("yyyyMMddHHmmss"), "完成", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show(this, "获取课表失败！\n请检查学号或密码是否正确。\n如果确认无误，可能是服务器升级了或爆炸了，请与作者联系。\n另外，假期时间无法获取课表。", "失败", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Prepare things that login process needs
+            hc.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36");
+            string strHtml = hc.GetStringAsync("http://ids.xidian.edu.cn/authserver/login?service=https%3A%2F%2Fxidian.cpdaily.com%2Fcomapp-timetable%2Fsys%2FschoolTimetable%2Fv2%2Fapi%2FweekTimetable").Result.ToString();
+            strLt = Regex.Match(strHtml, @"<input type=""hidden"" name=""lt"" value=""(.*?)""/>").Groups[1].Value;
+            strExecution = Regex.Match(strHtml, @"<input type=""hidden"" name=""execution"" value=""(.*?)""/>").Groups[1].Value;
+            strEventId = Regex.Match(strHtml, @"<input type=""hidden"" name=""_eventId"" value=""(.*?)""/>").Groups[1].Value;
+            strRmShown = Regex.Match(strHtml, @"<input type=""hidden"" name=""rmShown"" value=""(.*?)""/>").Groups[1].Value;
+            
+        }
+
+        public void AddToCalendar(string date, string courseName, string classroom, int sectionStart, int sectionEnd, bool isTimeB)
+        {
+            if (isTimeB)
+            {
+                CalendarEvent e = new CalendarEvent
+                {
+                    Start = new CalDateTime(DateTime.Parse(date + " " + sectionStartTime_B[sectionStart])),
+                    End = new CalDateTime(DateTime.Parse(date + " " + sectionEndTime_B[sectionEnd])),
+                    Description = courseName + " @ " + classroom,
+                    Summary = courseName + " @ " + classroom
+                };
+                timeTable.Events.Add(e);
+            }
+            else
+            {
+                CalendarEvent e = new CalendarEvent
+                {
+                    Start = new CalDateTime(DateTime.Parse(date + " " + sectionStartTime_A[sectionStart])),
+                    End = new CalDateTime(DateTime.Parse(date + " " + sectionEndTime_A[sectionEnd])),
+                    Description = courseName + " @ " + classroom,
+                    Summary = courseName + " @ " + classroom
+                };
+                timeTable.Events.Add(e);
+            }
+        }
+    }
+}
